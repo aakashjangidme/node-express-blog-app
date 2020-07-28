57e60c93abb923b441698c005570575e8cf6e730
Aakash Jangid <51054507+aakashjangidme@users.noreply.github.com>
2020-07-29 00:05:10 +0530

    added db mongo atlas and compose to post

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # node-express-blog-app
+
+* Stack - NodeJS, ExpressJS, EJs.
+* dB - MongoDB Atlas.
+* Need to RunMongoDB Atlas Shell to Access DB.
+
+[GitHub](http://github.com/aakashjangidme)
+
+![GitHub Logo](/images/logo.PNG)
--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 //jshint esversion:6
-
+//localhost/compose to compose the blog post!
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+//db stuff
+//run mongoatlas shell !
+const uri = "mongodb+srv://aakash-admin:skyyjaan@cluster0.xbhj9.mongodb.net/blogDB?retryWrites=true&w=majority";
+mongoose.connect(uri, {
+  useNewUrlParser: true
+});
+//TODO : remove password
+//schema
+const postSchema = {
+
+  title: String,
+
+  content: String
+
+};
+const Post = mongoose.model("Post", postSchema);
+
+
+const homeStartingContent = "Hi, I am Aakash. Welcome to my Blog Site";
+const aboutContent = "Hi, My name is Aakash Jangid and I am a Full Stack Developer from India. I have a passion for building web and mobile application from scratch to production. Please have a look at my project portfolio on my github at https://github.com/aakashjangidme";
+const contactContent = "Hi, My name is Aakash Jangid and I am a Full Stack Developer from India. You can reach me out on contact@beatsgram.studio";
 
 const app = express();
 
-posts = [];
+// posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -23,9 +42,18 @@ app.use(express.static("public"));
 
 app.get('/', function(req, res) {
 
-  res.render('home', {
-    posts : posts
+  Post.find({}, function(err, posts) {
+
+    res.render("home", {
+
+      startingContent: homeStartingContent,
+      posts: posts
+    });
   });
+
+  // res.render('home', {
+  //   posts : posts
+  // });
 });
 
 app.get('/about', function(req, res) {
@@ -56,33 +84,45 @@ app.get('/compose', function(req, res) {
 });
 
 app.post('/compose', function(req, res) {
-  const post = {
-    title : req.body.postTitle,
-    content : req.body.postText,
-  };
+  const post = new Post({
 
-  posts.push(post);
+    title: req.body.postTitle,
 
-  res.redirect('/');
-})
+    content: req.body.postText,
 
-app.get("/posts/:postName", function(req, res){
-  const requestedTitle = _.lowerCase(req.params.postName);
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-    if(storedTitle === requestedTitle){
-      res.render('post', {
-        title : post.title,
-        content : post.content,
-      });
-    }
-    else {
-      console.log('Not a match');
-    }
   });
 
-  console.log();
-})
+  // const post = {
+  //   title : req.body.postTitle,
+  //   content : req.body.postText,
+  // };
+
+  // posts.push(post);
+  post.save(function(err) {
+
+    if (!err) {
+
+      res.redirect("/");
+
+    }
+
+  });
+
+});
+
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
 
 
 app.listen(3000, function() {
